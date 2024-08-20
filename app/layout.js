353bc6b05726129366f3154104b35ce77d003c2b
Anthony Tommaso <atommaso05@gmail.com>
2024-08-20 15:15:47 -0400

    google analytics

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ClerkLoaded } from "@clerk/nextjs";
 import { ClerkProvider } from "@clerk/nextjs";
-import { Analytics } from "@vercel/analytics/react"
-
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,10 +13,25 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-      <Analytics />
-    </html>
+      <html lang="en">
+        <head>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-162Y73DG3Y"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-162Y73DG3Y');
+              `,
+            }}
+          />
+        </head>
+        <body className={inter.className}>
+          {children}
+          <Analytics />
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
